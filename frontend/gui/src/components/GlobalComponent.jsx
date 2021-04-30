cf9db37f98c9b4bfae38f547e74e8eab939d282d
Chinmay-395 <chinmaydali395@gmail.com>
2021-04-30 14:57:23 +0530

    added the chat-list UI

--- frontend/gui/src/components/GlobalComponent.jsx
+++ frontend/gui/src/components/GlobalComponent.jsx
@@ -1,131 +1,67 @@
 import React from "react";
-import {
-  Container,
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button,
-  Row,
-  Col,
-} from "reactstrap";
+// import {
+//   Container,
+//   Card,
+//   CardImg,
+//   CardText,
+//   CardBody,
+//   CardTitle,
+//   CardSubtitle,
+//   Button,
+//   Row,
+//   Col,
+// } from "reactstrap";
 import "../global/index2.scss";
 import shika from "../global/shikamaru.png";
 
-const UserListComponent = () => {
-  return (
-    <div class="cards">
-      <div class="outer">
-        <div class="card" style={{ "--delay": -1 }}>
-          <div class="content">
-            <div class="img">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAFIvBPH9fIMQZis4SHYTuv485uqr38bg1VQ&usqp=CAU" />
-            </div>
-            <div class="details">
-              <span class="name">Iron Man</span>
-              <p>Frontend Developer</p>
-            </div>
-          </div>
-          <a href="#">Follow</a>
-        </div>
-        <div class="card" style={{ "--delay": 0 }}>
-          <div class="content">
-            <div class="img">
+const CopyingHtml = () => (
+  <div className="block-61 space-between-blocks">
+    <div className="container">
+      <div className="row px-2 pt-4">
+        <div className="testimonial-card-1">
+          <div className="block-61-person">
+            <div>
               <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIMYmBQyoz9BWjEiMi5XipSPOmhamZUyI1gQ&usqp=CAU"
+                className="block-61-person__avatar"
+                src="https://uifaces.co/our-content/donated/1H_7AxP0.jpg"
                 alt=""
               />
             </div>
-            <div class="details">
-              <span class="name">Spider Man</span>
-              <p>Full-stack Developer</p>
+            <div className="px-2"></div>
+            <div className="flex-grow-1 d-flex flex-column">
+              <span className="block-61-person__name my-1">Ali Boukeroui</span>
+              <span className="block-61-person__info">frontendor.com</span>
             </div>
           </div>
-          <a href="#">Follow</a>
         </div>
-        <div class="card" style={{ "--delay": 1 }}>
-          <div class="content">
-            <div class="img">
+        <div className="testimonial-card-1">
+          <div className="block-61-person">
+            <div>
               <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe2k6vsAWhq5UPsCW58Q2RSCKqPxMDq75wfg&usqp=CAU"
+                className="block-61-person__avatar"
+                src="https://uifaces.co/our-content/donated/ukegoVAy.jpg"
                 alt=""
               />
             </div>
-            <div class="details">
-              <span class="name">Venom</span>
-              <p>Tester</p>
+            <div className="px-2"></div>
+            <div className="flex-grow-1 d-flex flex-column">
+              <span className="block-61-person__name my-1">Ali Boukeroui</span>
+              <span className="block-61-person__info">frontendor.com</span>
             </div>
           </div>
-          <a href="#">Follow</a>
         </div>
-        <div class="card" style={{ "--delay": 2 }}>
-          <div class="content">
-            <div class="img">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStwJX81iy7cnzgclyjTKlNnDlzIzPN1KSIgA&usqp=CAU"
-                alt=""
-              />
+        <div className="testimonial-card-1">
+          <div className="block-61-person">
+            <div>
+              <img className="block-61-person__avatar" src={shika} alt="" />
             </div>
-            <div class="details">
-              <span class="name">Batman</span>
-              <p>Backend Developer</p>
+            <div className="px-2"></div>
+            <div className="flex-grow-1 d-flex flex-column">
+              <span className="block-61-person__name my-1">Shikamaru Nara</span>
+              <span className="block-61-person__info">frontendor.com</span>
             </div>
           </div>
-          <a href="#">Follow</a>
         </div>
-        <div class="card" style={{ "--delay": 2 }}>
-          <div class="content">
-            <div class="img">
-              <img
-                src="https://static.rogerebert.com/uploads/review/primary_image/reviews/great-movie-superman-1978/EB20101104REVIEWS08101109987AR.jpg"
-                alt=""
-              />
-            </div>
-            <div class="details">
-              <span class="name">Superman</span>
-              <p>Youtuber</p>
-            </div>
-          </div>
-          <a href="#">Follow</a>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const UserCardComponent = () => (
-  <div class="box">
-    <div class="box-header">
-      <img
-        src={shika}
-        // style={{ width: "70%" }}
-        class="card-img-top h-10 w-100"
-        alt="..."
-      />
-      <span>USER NAME</span>
-    </div>
-  </div>
-);
-
-const UserProfileComponent = () => (
-  <div class="bs-example" style={{ margin: "20px" }}>
-    <div class="card" style={{ maxWidth: "500px" }}>
-      <div class="row no-gutters">
-        {/* <div class="col-sm-5" style={{ background: "#868e96" }}> */}
-        <img
-          src={shika}
-          // style={{ width: "70%" }}
-          class="card-img-top h-10 w-100"
-          alt="..."
-        />
-        {/* </div> */}
-        {/* <div class="col-sm-7"> */}
-        <div class="card-body">
-          <h5 class="card-title">Alice Liddel</h5>
-        </div>
-        {/* </div> */}
       </div>
     </div>
   </div>
@@ -144,140 +80,9 @@ function GlobalComponent() {
       }}
     >
       This will be a chat component
-      <div class="card">
-        {/* <img src="img_avatar.png" alt="Avatar" style="width:100%"/> */}
-        <div class="container">
-          <div class="box">
-            <div class="box-header">
-              <img
-                src={shika}
-                // style={{ width: "70%" }}
-                // class="card-img-top h-10 w-100"
-                alt="..."
-              />
-              <span>USER NAME</span>
-            </div>
-          </div>
-          <h4>
-            <b>John Doe</b>
-          </h4>
-          <p>Architect & Engineer</p>
-        </div>
-      </div>
-      <div class="card">
-        {/* <img src="img_avatar.png" alt="Avatar" style="width:100%"/> */}
-        <div class="container">
-          <h4>
-            <b>John Doe</b>
-          </h4>
-          <p>Architect & Engineer</p>
-        </div>
-      </div>
-      <div className="card">
-        {/* <img src="img_avatar.png" alt="Avatar" style="width:100%"/> */}
-        <div className="container">
-          {/* <UserCardComponent /> */}
-          <div className="d-flex flex-row border rounded">
-            <div className="p-0 w-25">
-              <img
-                // src="https://c1.staticflickr.com/3/2862/12328317524_18e52b5972_k.jpg"
-                src={shika}
-                className="img-thumbnail border-0"
-              />
-            </div>
-            <div className="pl-3 pt-2 pr-2 pb-2 w-75 border-left">
-              <h4 className="text-primary">Leanne Boulton</h4>
-              <h5 className="text-info">Photographer</h5>
-              <ul
-                className="m-0 float-left"
-                style={{ listStyle: "none", margin: 0, padding: 0 }}
-              >
-                <li>
-                  <i className="fab fa-facebook-square"></i> Facebook
-                </li>
-                <li>
-                  <i className="fab fa-twitter-square"></i> Twitter
-                </li>
-              </ul>
-              <p className="text-right m-0">
-                <a href="#" className="btn btn-primary">
-                  <i className="far fa-user"></i> View Profile
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          {/* <img src="img_avatar.png" alt="Avatar" style="width:100%"/> */}
-          <div className="container">
-            {/* <UserCardComponent /> */}
-            <div className="d-flex flex-row border rounded">
-              <div className="p-0 w-25">
-                <img
-                  // src="https://c1.staticflickr.com/3/2862/12328317524_18e52b5972_k.jpg"
-                  src={shika}
-                  className="img-thumbnail border-0"
-                />
-              </div>
-              <div className="pl-3 pt-2 pr-2 pb-2 w-75 border-left">
-                <h4 className="text-primary">Leanne Boulton</h4>
-                <h5 className="text-info">Photographer</h5>
-                <ul
-                  className="m-0 float-left"
-                  style={{ listStyle: "none", margin: 0, padding: 0 }}
-                >
-                  <li>
-                    <i className="fab fa-facebook-square"></i> Facebook
-                  </li>
-                  <li>
-                    <i className="fab fa-twitter-square"></i> Twitter
-                  </li>
-                </ul>
-                <p className="text-right m-0">
-                  <a href="#" className="btn btn-primary">
-                    <i className="far fa-user"></i> View Profile
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CopyingHtml />
     </div>
   );
 }
 
 export default GlobalComponent;
-
-/*
-<Card>
-        <CardBody>
-          <CardTitle tag="h5">
-            <div>person or group name you want to chat with</div>
-          </CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            <div>Some random subtitle</div>
-          </CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <CardTitle tag="h5">
-            <div>person or group name you want to chat with</div>
-          </CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            <div>Some random subtitle</div>
-          </CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-*/
